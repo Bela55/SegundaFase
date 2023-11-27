838580d9b969d38cfa61c88a822bd24debf0e7b7
Isabela Costa <142633329+Bela55@users.noreply.github.com>
2023-11-27 20:38:48 -0300

    Update index.js

--- src/pages/Calc/index.js
+++ src/pages/Calc/index.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import { ScrollView, View, Text, StyleSheet, Image } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { Appbar, Button } from 'react-native-paper';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Calculo = () => {
   const navigation = useNavigation();
-  const _goBack = () => console.log('Went back');
-  const _handleMore = () => console.log('Shown more');
+  const [currentItem, setCurrentItem] = useState(0);
+
+  const itemsList = [
+    'Volume do biodigestor: 000 m²',
+    'Altura total do biodigestor: 000 m',
+    'Diâmetro da câmara do biodigestor: 00 m',
+    'Altura da câmara do biodigestor: 00 m',
+    'Diâmetro do biodigestor: 00 m',
+    'Altura do gasômetro: 00 m',
+    'Comprimento do cano guia: 00 m',
+    'Dimensões dos tanques de carga e descarga: 00 m',
+    'Comprimento do cano de descarga: 00 m',
+    'Comprimento do cano de carga: 00 m',
+    'Volume de gás produzido pelo biodigestor: 00 m²',
+    'Data de criação: 17/09/2023'
+  ];
+
+  const handleNextItem = () => {
+    if (currentItem < itemsList.length - 1) {
+      setCurrentItem(currentItem + 1);
+    }
+  };
+
+  const handlePrevItem = () => {
+    if (currentItem > 0) {
+      setCurrentItem(currentItem - 1);
+    }
+  };
 
   return (
     <ScrollView style={styles.container}>
-      <Appbar.Header theme={{ colors: { primary: 'red' } }}>
-        <Appbar.BackAction onPress={() => navigation.goBack()} />
+      <Appbar.Header style={styles.header}>
+        <Appbar.BackAction  onPress={() => navigation.goBack() } style={styles.back}/>
         <Appbar.Content title="Calculo da Fazenda" titleStyle={styles.title} />
         <Ionicons
           name="menu-outline"
           size={24}
-          color="black"
-          onPress={_handleMore}
+          color="white"
+          onPress={() => console.log('Shown more')}
         />
       </Appbar.Header>
 
@@ -27,46 +52,30 @@ const Calculo = () => {
         <Text>Rancho da Serra</Text>
       </View>
 
+      <Image source={require('../../assets/animationvaca.json')} style={styles.image} />
+
       <View style={styles.content}>
-        <ScrollView>
-          <View style={styles.box}>
-            <Image source={''} style={styles.image} />
-            <Text style={styles.imageText}>Volume do biodigestor: 000 m²</Text>
-            <Text style={styles.imageText}>
-              Altura total do biodigestor: 000 m
-            </Text>
-            <Text style={styles.imageText}>
-              Diametro da câmara do biodigestor: 00 m
-            </Text>
-            <Text style={styles.imageText}>
-              Altura da câmara do biodigetor: 00 m
-            </Text>
-            <Text style={styles.imageText}>Diâmetro do biodigestor: 00 m</Text>
-            <Text style={styles.imageText}>Altura do gasômetro: 00 m</Text>
-            <Text style={styles.imageText}>Comprimento do cano guia: 00 m</Text>
-            <Text style={styles.imageText}>
-              Dimenssões dos tanques de carga e descarga: 00 m
-            </Text>
-            <Text style={styles.imageText}>
-              Comprimento do cano de descarga: 00 m
-            </Text>
-            <Text style={styles.imageText}>
-              Comprimento do cano de carga: 00 m
-            </Text>
-            <Text style={styles.textRed}>
-              Volume de gás produzido pelo biodigestor: 00 m²
-            </Text>
-            <Text style={styles.textRed}>Data de criação: 17/09/2023</Text>
-            <View style={styles.button}>
-              <Button
-                theme={{ colors: { primary: 'seagreen' } }}
-                mode="contained"
-                onPress={() => navigation.navigate('Inserir dados')}>
-                Novo Calculo
-              </Button>
-            </View>
-          </View>
-        </ScrollView>
+        <View style={styles.card}>
+          <Text style={styles.cardText}>{itemsList[currentItem]}</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={[styles.button, styles.prevButton]} onPress={handlePrevItem}>
+            <Text style={styles.buttonText}>Anterior</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.nextButton]} onPress={handleNextItem}>
+            <Text style={styles.buttonText}>Próximo</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.newCalcButton}>
+          <Button
+            theme={{ colors: { primary: 'seagreen' } }}
+            mode="contained"
+            onPress={() => navigation.navigate('Inserir dados')}
+            style={styles.newCalcButtonText}
+          >
+            Novo Cálculo
+          </Button>
+        </View>
       </View>
     </ScrollView>
   );
@@ -77,17 +86,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#2e8b57',
   },
+  header: {
+    backgroundColor: '#2e8b57', 
+  },
+  back: {
+    color: '#2e8b57', 
+  },
   title: {
     alignSelf: 'center',
     fontSize: 20,
     fontWeight: 'bold',
-  },
-  box: {
-    border: 2,
-    borderRadius: 10,
-    width: '100%',
-    backgroundColor: 'white',
-    alignItems: 'center',
+    color: 'white',
   },
   text1: {
     alignSelf: 'center',
@@ -103,31 +112,68 @@ const styles = StyleSheet.create({
     marginBottom: 30,
   },
   content: {
-    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+    
+  },
+  card: {
+    borderWidth: 1,
+    borderRadius: 5,
+    backgroundColor: 'white',
+    padding: 10,
+    marginHorizontal: 20,
+    marginBottom: 10,
+    minWidth: 300,
+  },
+  cardText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'left',
   },
   image: {
     width: 300,
     height: 200,
-    marginBottom: 20,
-    marginTop: 20,
     borderRadius: 10,
+    alignSelf: 'center',
+    marginTop: 20,
+    marginBottom: 10,
   },
-  imageText: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    textAlign: 'left',
-    marginLeft: 5,
-  },
-  textRed: {
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '70%',
     marginTop: 10,
-    fontSize: 14,
-    color: 'red',
-    textAlign: 'center',
-    marginBottom: 20,
   },
   button: {
-    marginBottom: 30,
+    borderWidth: 1,
+    padding: 8,
+    borderRadius: 5,
+    width: 100,
+    
+  },
+  prevButton: {
+    borderColor: 'white',
+  },
+  nextButton: {
+    borderColor: 'white',
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  newCalcButton: {
+    marginTop: 60,
+    borderWidth: 1,
+    borderRadius: 5,
+    width: '70%',
+    borderColor: 'white',
+  },
+  newCalcButtonText: {
+    color: 'white',
+    textAlign: 'center',
+    borderColor: 'white',
   },
 });
 
