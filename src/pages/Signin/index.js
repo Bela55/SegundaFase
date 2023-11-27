4761ad70b036ce5c658c10097ada452390730266
Isabela Costa <142633329+Bela55@users.noreply.github.com>
2023-11-27 20:45:56 -0300

    Update index.js

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import { useNavigation } from '@react-navigation/native';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function SignIn() {
   const navigation = useNavigation();
+  const [senha, setSenha] = useState('');
+  const [mostrarSenha, setMostrarSenha] = useState(false);
+
+  const toggleMostrarSenha = () => {
+    setMostrarSenha(!mostrarSenha);
+  };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
@@ -18,18 +24,30 @@ export default function SignIn() {
         <TextInput placeholder="Digite um email..." style={styles.input} />
 
         <Text style={styles.title}>Senha</Text>
-        <TextInput placeholder="Sua senha" style={styles.input} />
-
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText} onPress={() => navigation.navigate('Ola, Sr(a) Pessoa')}>Acessar</Text>
+        <View style={styles.inputSenhaContainer}>
+          <TextInput
+            placeholder="Sua senha"
+            style={styles.input}
+            secureTextEntry={!mostrarSenha}
+            value={senha}
+            onChangeText={(text) => setSenha(text)}
+          />
+          {/* Ícone do olho dentro do campo de senha */}
+          <TouchableOpacity style={styles.iconContainer} onPress={toggleMostrarSenha}>
+            <Icon name={mostrarSenha ? 'eye-slash' : 'eye'} size={20} color="#2e8b57" />
+          </TouchableOpacity>
+        </View>
+    
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Ola, Sr(a) Pessoa')}>
+          <Text style={styles.buttonText} >Acessar</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.buttonRegister}>
-          <Text style={styles.registerText} onPress={() => navigation.navigate('Cadastre-se')}>Nao possui uma conta? Cadastre-se</Text>
+        <TouchableOpacity style={styles.buttonRegister} onPress={() => navigation.navigate('Cadastre-se')}>
+          <Text style={styles.registerText} >Nao possui uma conta? Cadastre-se</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.buttonForgout}>
-          <Text style={styles.forgoutText} onPress={() => navigation.navigate('Redefinir a senha')}>Esqueci minha senha</Text>
+        <TouchableOpacity style={styles.buttonForgout} onPress={() => navigation.navigate('Redefinir a senha')}>
+          <Text style={styles.forgoutText} >Esqueci minha senha</Text>
         </TouchableOpacity>
       </Animatable.View>
     </ScrollView>
@@ -69,6 +87,9 @@ const styles = StyleSheet.create({
     marginBottom: 12,
     fontSize: 16,
   },
+  toggleButton: {
+    alignItems: 'flex-end',
+  },
   button: {
     backgroundColor: '#2e8b57',
     width: '100%',
@@ -98,4 +119,12 @@ const styles = StyleSheet.create({
   forgoutText: {
     color: '#2e8b57',
   },
-});
+  iconContainer: {
+    position: 'absolute',
+    right: 10,
+    top: 12,
+    zIndex: 1,
+  },
+  inputSenhaContainer: {
+    position: 'relative',
+  },
